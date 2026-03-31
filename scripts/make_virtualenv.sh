c2029e34c574164679ead6b5e0ffe0994a1a38ab
Joelle <167835963+JoeJams@users.noreply.github.com>
2026-03-31 08:51:14 +0200

    add_original back as unix backup

--- scripts/make_virtualenv.sh
+++ scripts/make_virtualenv.sh
@@ -4,14 +4,13 @@
 # pip install virtualenv
 
 scripts=$(dirname "$0")
-base=$(realpath $scripts/..)
+base=$scripts/..
 
 mkdir -p $base/venvs
 
 # python3 needs to be installed on your system
-# removed '3' for my system as it doesn't work otherwise
 
-python -m virtualenv -p python $base/venvs/torch3
+python3 -m virtualenv -p python3 $base/venvs/torch3
 
 echo "To activate your environment:"
-echo "source $base/venvs/torch3/Scripts/activate"
+echo "    source $base/venvs/torch3/bin/activate"
